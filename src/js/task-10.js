687d9ea1def5003e6d955e222c827c92859427b5
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-10-31 23:33:00 +0200

    +task10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  const inputNumber = document.querySelector("#controls").firstElementChild;
+  const btnDestroy = document.querySelector("#controls").lastElementChild;
+  const btnCreate = btnDestroy.previousElementSibling;
+  const divBoxes = document.querySelector("div#boxes");
+
+  inputNumber.addEventListener("input", onInputNumberChange);
+  btnCreate.addEventListener("click", onbtnCreateClick);
+  btnDestroy.addEventListener("click", onBtnDestroyClick);
+
+  function onInputNumberChange(event) {
+    amount = event.target.value;
+  }
+
+  function onbtnCreateClick(event) {
+    let size = 20;
+    let boxesArr = [];
+    for (let i = 0; i < amount; i++) {
+      let colorBox = getRandomHexColor();
+      size += 10;
+      let oneBox = `<div style="width: ${size}px; height: ${size}px; background-color: ${colorBox};"></div>`;
+      boxesArr.push(oneBox);
+    }
+    divBoxes.insertAdjacentHTML("afterbegin", boxesArr.join(""));
+  }
+
+  function onBtnDestroyClick(event) {
+    divBoxes.innerHTML = "";
+  }
+}
+createBoxes();
