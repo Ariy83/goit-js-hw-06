12545f46f3651b1ac9396ec55f9a63646343fe6b
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-10-31 21:31:32 +0200

    +task08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const loginFormElem = document.querySelector("form.login-form");
 
+loginFormElem.addEventListener("submit", onloginFormElemSubmit);
+
+function onloginFormElemSubmit(event) {
+  event.preventDefault();
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+  const result = { email, password };
+
+  if (email && password) {
+    console.log(result);
+  } else {
+    alert("Всі поля повинні бути заповнені");
+  }
+
+  event.target.reset();
+}
